4d7a19db2e90dab8c212e8149927b742e40c8f68
Airhon Cortez <airhon@mangodream.us>
2023-07-03 14:55:04 +0800

    Adds open api specs

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [LoginController::class, 'login']);
 
 Route::post('/ip-address', [IpAddressController::class, 'store']);
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
